50cf96b7355ea5a23f114fb349c09cf1b2970be7
ptangen <ptangen@ptangen.com>
2016-12-18 17:23:12 -0500

    Initial Commit

---
+++ EquityStatus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EquityStatus
+//
+//  Created by Paul Tangen on 12/18/16.
+//  Copyright © 2016 Paul Tangen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
