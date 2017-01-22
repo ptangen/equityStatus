2f5614e50d79d5d6a633327826e639fa83bc19d9
ptangen <ptangen@ptangen.com>
2017-01-22 13:02:53 -0500

    Remove the MSSimpleGauge cocoapod.

--- Pods/Target Support Files/Pods-EquityStatus/Pods-EquityStatus-frameworks.sh
+++ Pods/Target Support Files/Pods-EquityStatus/Pods-EquityStatus-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MSSimpleGauge/MSSimpleGauge.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Charts/Charts.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/MSSimpleGauge/MSSimpleGauge.framework"
 fi
