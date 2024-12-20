13aa08ac66be60c7df204cdfb4e9ce9aa36774fd
Paul <ptangen@ptangen.com>
2024-12-20 09:17:20 -0500

    Remove v1 files

--- Pods/Target Support Files/Pods-EquityStatus/Pods-EquityStatus-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EquityStatus : NSObject
-@end
-@implementation PodsDummy_Pods_EquityStatus
-@end
--- Pods/Target Support Files/Pods-EquityStatusTests/Pods-EquityStatusTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_EquityStatusTests : NSObject
-@end
-@implementation PodsDummy_Pods_EquityStatusTests
-@end
